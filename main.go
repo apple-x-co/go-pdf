47bfe1d2fa596465312d06a11a51be883c15c9cc
Kouhei Sano <sano@buddying.jp>
2019-04-16 11:02:45 +0900

    go-pdf

--- main.go
+++ main.go
@@ -46,16 +46,16 @@ func main() {
 		return
 	}
 
-	var decoded types.LinerLayout
+	var page types.Page
 	bytes := []byte(string(b))
-	if err := json.Unmarshal(bytes, &decoded); err != nil {
+	if err := json.Unmarshal(bytes, &page); err != nil {
 		fmt.Println("error:", err)
 		return
 	}
-	//fmt.Printf("%v\n", decoded)
+	//fmt.Printf("%v\n", page)
 
 	pdf := gopdf.GoPdf{}
-	pdf.Start(gopdf.Config{PageSize: *gopdf.PageSizeA4})
+	pdf.Start(gopdf.Config{PageSize: gopdf.Rect{W: page.Width, H: page.Height}})
 	if err := pdf.AddTTFFont("default", *ttfPath); err != nil {
 		log.Print(err.Error())
 		return
@@ -65,7 +65,14 @@ func main() {
 		return
 	}
 
-	drawPdf(&pdf, decoded)
+	left, top, right, bottom := pdf.Margins()
+	fmt.Printf("left: %v\n", left)
+	fmt.Printf("top: %v\n", top)
+	fmt.Printf("right: %v\n", right)
+	fmt.Printf("bottom: %v\n", bottom)
+
+	pdf.AddPage()
+	drawPdf(&pdf, page, page.LinerLayout)
 
 	if err := pdf.WritePdf(*outputPath); err != nil {
 		log.Print(err.Error())
@@ -75,26 +82,38 @@ func main() {
 	_ = pdf.Close()
 }
 
-func drawPdf(pdf *gopdf.GoPdf, linerLayout types.LinerLayout) {
+func drawPdf(pdf *gopdf.GoPdf, page types.Page, linerLayout types.LinerLayout) {
 
 	fmt.Printf("orientation: %v\n", linerLayout.Orientation)
 
-	pdf.AddPage() // todo: 出力ごとに今の高さを調べて、必要なときに自動改行を行うようにする
+	//x := pdf.GetX()
+	//y := pdf.GetY()
+	//width := page.Width - pdf.MarginLeft() - pdf.MarginRight()
+	//height := page.Height - pdf.MarginTop() - pdf.MarginBottom()
 
 	for _, element := range linerLayout.Elements {
 		switch element.Type {
+		case "line_break":
+			var decoded types.ElementLineBreak
+			_ = json.Unmarshal(element.Attributes, &decoded)
+			pdf.Br(decoded.Height)
+
 		case "text":
+			// todo: 文字描画に必要な矩形を取得
+			// todo: 現在の座標位置と縦横サイズと矩形を比較して、次の行に入る場合は改行。入らない場合は改ページをする
 			var decoded types.ElementText
 			_ = json.Unmarshal(element.Attributes, &decoded)
 			_ = pdf.Cell(nil, decoded.Text)
+
 		case "image":
 			var decoded types.ElementImage
 			_ = json.Unmarshal(element.Attributes, &decoded)
-			_ = pdf.Cell(nil, decoded.Path)
+			//_ = pdf.Cell(nil, decoded.Path)
+			_ = pdf.Image(decoded.Path, 200, 50, nil)
 		}
 	}
 
 	for _, linerLayout := range linerLayout.LinearLayouts {
-		drawPdf(pdf, linerLayout)
+		drawPdf(pdf, page, linerLayout)
 	}
 }
--- types/types.go
+++ types/types.go
@@ -2,15 +2,43 @@ package types
 
 import "encoding/json"
 
+type Page struct {
+	Width       float64     `json:"width"`
+	Height      float64     `json:"height"`
+	LineHeight  float64     `json:"line_height"`
+	LinerLayout LinerLayout `json:"liner_layout"`
+}
+
 type LinerLayout struct {
 	Orientation   string        `json:"orientation"`
 	LinearLayouts []LinerLayout `json:"linear_layouts"`
 	Elements      []Element     `json:"elements"`
 }
 
+func (linerLayout *LinerLayout) IsVertical() bool {
+	return linerLayout.Orientation == "vertical"
+}
+func (linerLayout *LinerLayout) IsHorizontal() bool {
+	return linerLayout.Orientation == "horizontal"
+}
+
 type Element struct {
 	Type       string          `json:"type"`
 	Attributes json.RawMessage `json:"attributes"`
+}
+
+func (element *Element) IsLineBreak() bool {
+	return element.Type == "line_break"
+}
+func (element *Element) IsText() bool {
+	return element.Type == "text"
+}
+func (element *Element) IsImage() bool {
+	return element.Type == "image"
+}
+
+type ElementLineBreak struct {
+	Height float64 `json:"height"`
 }
 
 type ElementText struct {
