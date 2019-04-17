bb9713375a6a340cdb2751cc3b3bc708fdbfcd8f
Kouhei Sano <sano@buddying.jp>
2019-04-17 16:12:57 +0900

    go-pdf

--- main.go
+++ main.go
@@ -93,13 +93,7 @@ func drawPdf(gp *gopdf.GoPdf, pdf types.PDF, linerLayout types.LinerLayout) {
 
 	//fmt.Printf("orientation: %v\n", linerLayout.Orientation)
 
-	width := pdf.Width - gp.MarginLeft() - gp.MarginRight()
-	height := pdf.Height - gp.MarginTop() - gp.MarginBottom()
-
 	for _, element := range linerLayout.Elements {
-		x := gp.GetX()
-		//y := gp.GetY()
-
 		switch element.Type {
 		case "line_break":
 			var decoded types.ElementLineBreak
@@ -109,116 +103,128 @@ func drawPdf(gp *gopdf.GoPdf, pdf types.PDF, linerLayout types.LinerLayout) {
 		case "text":
 			var decoded = types.ElementText{Color: types.Color{R: pdf.TextColor.R, G: pdf.TextColor.G, B: pdf.TextColor.B}, Width: -1, Height: -1, Border: types.Border{Width: -1, Color: types.Color{R: 0, B: 0, G: 0}}}
 			_ = json.Unmarshal(element.Attributes, &decoded)
-
-			var textRect gopdf.Rect
-
-			if decoded.Width != -1 && decoded.Height != -1 {
-				textRect = gopdf.Rect{W: decoded.Width, H: decoded.Height}
-			} else if decoded.Width != -1 && decoded.Height == -1 {
-				measureHeight := pdf.TextHeight() * (float64(pdf.TextSize) / 1000.0)
-				textRect = gopdf.Rect{W: decoded.Width, H: measureHeight}
-			} else if decoded.Width == -1 && decoded.Height != -1 {
-				measureWidth, _ := gp.MeasureTextWidth(decoded.Text)
-				textRect = gopdf.Rect{W: measureWidth, H: decoded.Height}
-			} else {
-				measureWidth, _ := gp.MeasureTextWidth(decoded.Text)
-				measureHeight := pdf.TextHeight() * (float64(pdf.TextSize) / 1000.0)
-				textRect = gopdf.Rect{W: measureWidth, H: measureHeight}
-			}
-
-			gp.SetTextColor(decoded.Color.R, decoded.Color.G, decoded.Color.B)
-
-			if decoded.Border.Width != -1 {
-				gp.SetLineWidth(decoded.Border.Width)
-				gp.SetStrokeColor(decoded.Border.Color.R, decoded.Border.Color.G, decoded.Border.Color.B)
-				gp.Line(gp.GetX(), gp.GetY(), gp.GetX()+textRect.W, gp.GetY())
-				gp.Line(gp.GetX()+textRect.W, gp.GetY(), gp.GetX()+textRect.W, gp.GetY()+textRect.H)
-				gp.Line(gp.GetX()+textRect.W, gp.GetY()+textRect.H, gp.GetX(), gp.GetY()+textRect.H)
-				gp.Line(gp.GetX(), gp.GetY()+textRect.H, gp.GetX(), gp.GetY())
-			}
-
-			if linerLayout.IsHorizontal() {
-				if x+textRect.W > width {
-					if lineHeight := linerLayout.LineHeight; lineHeight != 0 {
-						gp.Br(lineHeight)
-					} else if lineHeight := pdf.LineHeight; lineHeight != 0 {
-						gp.Br(lineHeight)
-					} else {
-						gp.Br(20)
-					}
-				}
-
-				if gp.GetY()+textRect.H > height && pdf.AutoPageBreak {
-					gp.AddPage()
-				}
-
-				_ = gp.Cell(&textRect, decoded.Text)
-			} else if linerLayout.IsVertical() {
-				if gp.GetY()+textRect.H > height && pdf.AutoPageBreak {
-					gp.AddPage()
-				}
-
-				_ = gp.Cell(&textRect, decoded.Text)
-				gp.SetX(gp.MarginLeft())
-				gp.SetY(gp.GetY() + linerLayout.LineHeight)
-			}
-
-			gp.SetTextColor(pdf.TextColor.R, pdf.TextColor.G, pdf.TextColor.B)
+			drawText(gp, pdf, linerLayout, decoded)
 
 		case "image":
 			var decoded = types.ElementImage{X: -1, Y: -1, Width: -1, Height: -1}
 			_ = json.Unmarshal(element.Attributes, &decoded)
-
-			imageRect := gopdf.Rect{}
-			if decoded.Width != -1 && decoded.Height != -1 {
-				imageRect.W = decoded.Width
-				imageRect.H = decoded.Height
-			} else if decoded.Width == -1 && decoded.Height == -1 {
-				file, _ := os.Open(decoded.Path)
-				img, _, _ := image.DecodeConfig(file)
-				imageRect.W = float64(img.Width)
-				imageRect.H = float64(img.Height)
-			} else if decoded.Width == -1 && decoded.Height != -1 {
-				file, _ := os.Open(decoded.Path)
-				img, _, _ := image.DecodeConfig(file)
-				imageRect.H = decoded.Height
-				imageRect.W = float64(img.Width) * (imageRect.H / float64(img.Height))
-			} else if decoded.Width != -1 && decoded.Height == -1 {
-				file, _ := os.Open(decoded.Path)
-				img, _, _ := image.DecodeConfig(file)
-				imageRect.W = decoded.Width
-				imageRect.H = float64(img.Height) * (imageRect.W / float64(img.Width))
-			}
-
-			if gp.GetX()+imageRect.W > pdf.Width {
-				if lineHeight := linerLayout.LineHeight; lineHeight != 0 {
-					gp.Br(lineHeight)
-				} else if lineHeight := pdf.LineHeight; lineHeight != 0 {
-					gp.Br(lineHeight)
-				} else {
-					gp.Br(20)
-				}
-			}
-
-			if gp.GetY()+imageRect.H > height && pdf.AutoPageBreak {
-				gp.AddPage()
-			}
-
-			if decoded.X != -1 || decoded.Y != -1 {
-				_ = gp.Image(decoded.Path, decoded.X, decoded.Y, &imageRect)
-			} else {
-				_ = gp.Image(decoded.Path, gp.GetX(), gp.GetY(), &imageRect)
-
-				if linerLayout.IsHorizontal() {
-					gp.SetX(gp.GetX() + imageRect.W)
-				} else if linerLayout.IsVertical() {
-					gp.SetY(gp.GetY() + imageRect.H)
-				}
-			}
+			drawImage(gp, pdf, linerLayout, decoded)
 		}
 	}
 
 	for _, linerLayout := range linerLayout.LinearLayouts {
 		drawPdf(gp, pdf, linerLayout)
+	}
+}
+
+func drawText(gp *gopdf.GoPdf, pdf types.PDF, linerLayout types.LinerLayout, decoded types.ElementText) {
+	x := gp.GetX()
+	width := pdf.Width - gp.MarginLeft() - gp.MarginRight()
+	height := pdf.Height - gp.MarginTop() - gp.MarginBottom()
+
+	var textRect gopdf.Rect
+
+	if decoded.Width != -1 && decoded.Height != -1 {
+		textRect = gopdf.Rect{W: decoded.Width, H: decoded.Height}
+	} else if decoded.Width != -1 && decoded.Height == -1 {
+		measureHeight := pdf.TextHeight() * (float64(pdf.TextSize) / 1000.0)
+		textRect = gopdf.Rect{W: decoded.Width, H: measureHeight}
+	} else if decoded.Width == -1 && decoded.Height != -1 {
+		measureWidth, _ := gp.MeasureTextWidth(decoded.Text)
+		textRect = gopdf.Rect{W: measureWidth, H: decoded.Height}
+	} else {
+		measureWidth, _ := gp.MeasureTextWidth(decoded.Text)
+		measureHeight := pdf.TextHeight() * (float64(pdf.TextSize) / 1000.0)
+		textRect = gopdf.Rect{W: measureWidth, H: measureHeight}
+	}
+
+	gp.SetTextColor(decoded.Color.R, decoded.Color.G, decoded.Color.B)
+
+	if decoded.Border.Width != -1 {
+		gp.SetLineWidth(decoded.Border.Width)
+		gp.SetStrokeColor(decoded.Border.Color.R, decoded.Border.Color.G, decoded.Border.Color.B)
+		gp.Line(gp.GetX(), gp.GetY(), gp.GetX()+textRect.W, gp.GetY())
+		gp.Line(gp.GetX()+textRect.W, gp.GetY(), gp.GetX()+textRect.W, gp.GetY()+textRect.H)
+		gp.Line(gp.GetX()+textRect.W, gp.GetY()+textRect.H, gp.GetX(), gp.GetY()+textRect.H)
+		gp.Line(gp.GetX(), gp.GetY()+textRect.H, gp.GetX(), gp.GetY())
+	}
+
+	if linerLayout.IsHorizontal() {
+		if x+textRect.W > width {
+			if lineHeight := linerLayout.LineHeight; lineHeight != 0 {
+				gp.Br(lineHeight)
+			} else if lineHeight := pdf.LineHeight; lineHeight != 0 {
+				gp.Br(lineHeight)
+			} else {
+				gp.Br(20)
+			}
+		}
+
+		if gp.GetY()+textRect.H > height && pdf.AutoPageBreak {
+			gp.AddPage()
+		}
+
+		_ = gp.Cell(&textRect, decoded.Text)
+	} else if linerLayout.IsVertical() {
+		if gp.GetY()+textRect.H > height && pdf.AutoPageBreak {
+			gp.AddPage()
+		}
+
+		_ = gp.Cell(&textRect, decoded.Text)
+		gp.SetX(gp.MarginLeft())
+		gp.SetY(gp.GetY() + linerLayout.LineHeight)
+	}
+
+	gp.SetTextColor(pdf.TextColor.R, pdf.TextColor.G, pdf.TextColor.B)
+}
+
+func drawImage(gp *gopdf.GoPdf, pdf types.PDF, linerLayout types.LinerLayout, decoded types.ElementImage) {
+	height := pdf.Height - gp.MarginTop() - gp.MarginBottom()
+
+	imageRect := gopdf.Rect{}
+	if decoded.Width != -1 && decoded.Height != -1 {
+		imageRect.W = decoded.Width
+		imageRect.H = decoded.Height
+	} else if decoded.Width == -1 && decoded.Height == -1 {
+		file, _ := os.Open(decoded.Path)
+		img, _, _ := image.DecodeConfig(file)
+		imageRect.W = float64(img.Width)
+		imageRect.H = float64(img.Height)
+	} else if decoded.Width == -1 && decoded.Height != -1 {
+		file, _ := os.Open(decoded.Path)
+		img, _, _ := image.DecodeConfig(file)
+		imageRect.H = decoded.Height
+		imageRect.W = float64(img.Width) * (imageRect.H / float64(img.Height))
+	} else if decoded.Width != -1 && decoded.Height == -1 {
+		file, _ := os.Open(decoded.Path)
+		img, _, _ := image.DecodeConfig(file)
+		imageRect.W = decoded.Width
+		imageRect.H = float64(img.Height) * (imageRect.W / float64(img.Width))
+	}
+
+	if gp.GetX()+imageRect.W > pdf.Width {
+		if lineHeight := linerLayout.LineHeight; lineHeight != 0 {
+			gp.Br(lineHeight)
+		} else if lineHeight := pdf.LineHeight; lineHeight != 0 {
+			gp.Br(lineHeight)
+		} else {
+			gp.Br(20)
+		}
+	}
+
+	if gp.GetY()+imageRect.H > height && pdf.AutoPageBreak {
+		gp.AddPage()
+	}
+
+	if decoded.X != -1 || decoded.Y != -1 {
+		_ = gp.Image(decoded.Path, decoded.X, decoded.Y, &imageRect)
+	} else {
+		_ = gp.Image(decoded.Path, gp.GetX(), gp.GetY(), &imageRect)
+
+		if linerLayout.IsHorizontal() {
+			gp.SetX(gp.GetX() + imageRect.W)
+		} else if linerLayout.IsVertical() {
+			gp.SetY(gp.GetY() + imageRect.H)
+		}
 	}
 }
